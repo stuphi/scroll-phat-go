aeea8ba00cdff5f2de933aa793a08c02b582e2f6
stuphi <philip@stuphi.co.uk>
2015-12-05 19:53:09 +0000

    Tidied up a bit.

--- scrollphat.go
+++ scrollphat.go
@@ -12,9 +12,14 @@ const CMD_SET_MODE = 0x00
 const CMD_SET_BRIGHTNESS = 0x19
 const MODE_5X11 = 3  //0b00000011
 
+type ScrollPhat struct {
+    buffer []byte
+    device hwio.I2CDevice
+    offset int
+}
 
-func main(){
-    
+func (s *ScrollPhat) Init() {
+
     m, e := hwio.GetModule("i2c")
     if e != nil {
         fmt.Printf("could not get i2c module: %s\n", e)
@@ -26,60 +31,67 @@ func main(){
     // as the default device tree enables it.
 
     i2c.Enable()
-    defer i2c.Disable()
+    //defer i2c.Disable()
 
-    device := i2c.GetDevice(I2C_ADDR)
+    s.device = i2c.GetDevice(I2C_ADDR)
 
-    e = device.WriteByte(CMD_SET_MODE, MODE_5X11)
+    e = s.device.WriteByte(CMD_SET_MODE, MODE_5X11)
     if e != nil {
         fmt.Printf("could not write Byte: %s\n", e)
     }
 
-    e = device.WriteByte(CMD_SET_BRIGHTNESS, 2)
+    e = s.device.WriteByte(CMD_SET_BRIGHTNESS, 2)
     if e != nil {
         fmt.Printf("could not set brightness: %s\n", e)
     }
 
-    myData := []byte{0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF }
+    s.offset = 0
+}
 
-    e = device.Write(0x01, myData)
+func (s *ScrollPhat) Update() {
+    var window []byte
+    if s.offset > len(s.buffer){
+        s.offset = 0
+    }
+    if s.offset + 11 <= len(s.buffer) {
+        window = s.buffer[s.offset:s.offset + 11]
+    } else {
+        window = s.buffer[s.offset:]
+        window = append(window, s.buffer[:11 - len(window)]...)
+    }
+    //window = append(window, 0xFF)
+
+    e := s.device.Write(0x01, window)
     if e != nil {
         fmt.Printf("could not write Byte Array: %s\n", e)
     }
-
-    e = device.WriteByte(0x0C, 0)
+    
+    e = s.device.WriteByte(0x0C, 0)
     if e != nil {
         fmt.Printf("could not update column register: %s\n", e)
     }
+}
 
-    time.Sleep(500 * time.Millisecond)
+func main() {
 
-    myData = []byte{0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00}
+    var sf ScrollPhat
 
-    e = device.Write(0x01, myData)
-    if e != nil {
-        fmt.Printf("could not write Byte Array: %s\n", e)
+    sf.Init()
+
+    //sf.buffer = []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10 }
+    sf.buffer = []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+                       0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+                       0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+                       0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F,
+                   }
+    for i:=0;i<=100;i++ {
+        sf.offset = sf.offset + 1
+        sf.Update()
+        time.Sleep(500 * time.Millisecond)
     }
 
-    e = device.WriteByte(0x0C, 0)
-    if e != nil {
-        fmt.Printf("could not update column register: %s\n", e)
-    }
-
-    time.Sleep(500 * time.Millisecond)
-
-    myData = []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
-
-    e = device.Write(0x01, myData)
-    if e != nil {
-        fmt.Printf("could not write Byte Array: %s\n", e)
-    }
-
-    e = device.WriteByte(0x0C, 0)
-    if e != nil {
-        fmt.Printf("could not update column register: %s\n", e)
-    }
-
-
+    sf.buffer = []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }
+    sf.offset = 0
+    sf.Update()
 }
 
